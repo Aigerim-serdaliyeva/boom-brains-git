0e564a579d458d739b835cda32bc96b83f7e4eeb
Айгерим Сердалиева <aigerim.serdaliyeva@mail.ru>
2019-10-17 14:47:30 +0600

    fixed

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -6,23 +6,15 @@
 
 <script>
 export default {
-  props: {
-    // columnCount: {
-    //   type: Number,
-    //   default: 4
-    // }      
+  props: {    
   }
 }
 </script>
 
 <style lang="scss">
   .grid {
-    // display: grid;
-    // grid-gap: 5px;
     position: relative;
     width: 100%;
-    height: 100%;
-    min-height: 500px;
-    // height: 500px;
+    height: 400px;
   }
 </style>
