5c94fc8676ef5679a5216b61a6f3c54cfc83e8bb
Айгерим Сердалиева <aigerim.serdaliyeva@mail.ru>
2019-10-18 17:49:36 +0600

    position absolute

--- src/components/widget/Widget.vue
+++ src/components/widget/Widget.vue
@@ -1,34 +1,33 @@
 <template>
-    <div class="widget">
-        <h4 class="widget__title">{{ title }}</h4>
-        <div class="widget__content">
-            <slot></slot>
-        </div>
+  <div class="widget">
+    <h4 class="widget__title">{{title}}</h4>
+    <div class="widget__content">
+      <slot></slot>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        title: String
-    }
-};
+  props: {
+    title: String
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-.widget {
-    background: #f1f1f1;
+  .widget {    
+    position: absolute;
+    background: #F1F1F1;
     border-radius: 5px;
     padding: 20px 25px;
-    margin-bottom: 5px;
-    // height: 100%;
-    // border: 2px solid #fff;
-
+    height: 100%;
+    border: 2px solid #fff;
     &__title {
-        text-align: center;
-        font-size: 13px;
-        font-weight: normal;
-        color: #888;
+      text-align: center;
+      font-size: 13px;
+      font-weight: normal;
+      color: #888;
     }
-}
+  }
 </style>
--- src/pages/profile/ProfileMenu.vue
+++ src/pages/profile/ProfileMenu.vue
@@ -1,131 +1,56 @@
 <template>
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="row">
-                <div class="col-lg-5">
-                    <Widget title="Недельная активность">
-                        <router-link to="/profile/week">
-                            text
-                        </router-link>
-                    </Widget>
-                </div>
-                <div class="col-lg-5">
-                    <Widget title="Прогресс за месяц">
-                        <router-link to="/profile/month">
-                            text
-                        </router-link>
-                    </Widget>
-                </div>
-                <div class="col-lg-5">
-                    <Widget title="Данные по играм">
-                        <router-link to="/profile/game-data">
-                            text
-                        </router-link>
-                    </Widget>
-                </div>
-            </div>
-           <div class="row">
-               <div class="col-lg-10">
-                   <Widget class='h-100' title="Radar">
-                        <router-link to="/profile/radar">
-                            text
-                        </router-link>
-                    </Widget>
-               </div>
-               <div class="col-lg-5">
-                   <Widget title="Возрастная статистика">
-                        <router-link to="/profile/age">
-                            text
-                        </router-link>
-                    </Widget>
-                    <Widget title="Рекорды">
-                        <router-link to="/profile/records">
-                            text
-                        </router-link>
-                    </Widget>
-               </div>
-           </div>
-        </div>
-        <div class="col-lg-4">
-            <Widget title="Рейтинг среди друзей">
-                <router-link to="/profile/rating-friends">
-                    text
+    <div>
+        <Grid>
+            <Widget title="Недельная активность" :style="{'top': '0', 'left': '0', 'height': '25%', 'width': '25%'}">
+                <router-link to="/profile/week">    
+                text
                 </router-link>
             </Widget>
-
-            <Widget title="Мировой рейтинг">
-                <router-link to="/profile/rating-world">
-                    text
+            <Widget title="Прогресс за месяц" :style="{'top': '0', 'left': '25%', 'height': '25%', 'width': '25%'}">
+                <router-link to="/profile/month">    
+                text     
                 </router-link>
             </Widget>
-        </div>
-
-        <!-- <div class="d-flex">
-            <div>
-                <div class="d-flex ">
-                    <Widget title="Недельная активность">
-                        <router-link to="/profile/week">
-                            text
-                        </router-link>
-                    </Widget>
-                    <Widget title="Прогресс за месяц">
-                        <router-link to="/profile/month">
-                            text
-                        </router-link>
-                    </Widget>
-                    <Widget title="Данные по играм">
-                        <router-link to="/profile/game-data">
-                            text
-                        </router-link>
-                    </Widget>
-                </div>
-                <div>
-                    <div class="d-flex">
-                        <div class='flex-fill'>
-                            <Widget class='h-100' title="Radar">
-                                <router-link to="/profile/radar">
-                                    text
-                                </router-link>
-                            </Widget>
-                        </div>
-                        
-                        <div class='d-flex flex-column flex-fill'>
-                            <Widget title="Возрастная статистика">
-                                <router-link to="/profile/age">
-                                    text
-                                </router-link>
-                            </Widget>
-                            <Widget title="Рекорды">
-                                <router-link to="/profile/records">
-                                    text
-                                </router-link>
-                            </Widget>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <Widget title="Рейтинг среди друзей">
-                    <router-link to="/profile/rating-friends">
-                        text
-                    </router-link>
-                </Widget>
-
-                <Widget title="Мировой рейтинг">
-                    <router-link to="/profile/rating-world">
-                        text
-                    </router-link>
-                </Widget>
-            </div>
-        </div> -->
+            <Widget title="Данные по играм" :style="{'top': '0', 'left': '50%', 'height': '25%', 'width': '25%'}">
+                <router-link to="/profile/game-data"> 
+                text        
+                </router-link>
+            </Widget>
+            <Widget title="Рейтинг среди друзей" :style="{'top': '0', 'left': '75%', 'height': '50%', 'width': '25%'}">
+                <router-link to="/profile/rating-friends">  
+                text       
+                </router-link>
+            </Widget>
+            <Widget title="Radar" :style="{'top': '25%', 'left': '0', 'height': '75%', 'width': '50%'}">
+                <router-link to="/profile/radar">  
+                text         
+                </router-link>
+            </Widget>
+            <Widget title="Возрастная статистика" :style="{'top': '25%', 'left': '50%', 'height': '37.5%', 'width': '25%'}">
+                <router-link to="/profile/age">   
+                text      
+                </router-link>
+            </Widget>
+            <Widget title="Рекорды" :style="{'top': '62.5%', 'left': '50%', 'height': '37.5%', 'width': '25%'}">
+                <router-link to="/profile/records">  
+                text       
+                </router-link>
+            </Widget>
+            <Widget title="Мировой рейтинг" :style="{'top': '50%', 'left': '75%', 'height': '50%', 'width': '25%'}">
+                <router-link to="/profile/rating-world">   
+                text      
+                </router-link>
+            </Widget>      
+        </Grid>
     </div>
 </template>
 
 <script>
+import Grid from '../../components/grid/Grid.vue'
 import Widget from "../../components/widget/Widget";
 export default {
     components: {
-        Widget
+        Widget, Grid
     }
 };
 </script>
